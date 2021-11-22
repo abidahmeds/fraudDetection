16b9eb8682c37a95f15a6add884d43e75239e124
abidahmeds <abidahmeds@gmail.com>
2021-11-22 12:55:12 +0530

    added 3 new ML model code

--- model.py
+++ model.py
@@ -1,1 +1,10 @@
 print "entire code related to machine learning model"
+
+def model1():
+    print 'adding code for ML model1'
+
+def model2():
+    print 'adding code for ML model2'
+
+def model3():
+    print 'adding code for ML model3'
